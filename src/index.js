29c370112c43cc73fd013eee2028406b7a322b44
Creator Rama <ap410485@gmail.com>
2025-02-01 11:23:12 +0530

    added new migration to add foreign key constraint in one to many relation

--- src/index.js
+++ src/index.js
@@ -7,7 +7,19 @@ const apiRoutes=require('./routes')
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use('/api',apiRoutes)
-app.listen(serverconfig.PORT,()=>{
+app.listen(serverconfig.PORT,async ()=>{
     console.log(`successfully started the server on port ${serverconfig.PORT}`);
     // logger.info('successfully started the server')
+    const {City,Airport}=require('./models')
+    // const noida=await City.findByPk(1);
+    // const airport=await Airport.create({name:'lal bahadur shatri international airport',code:'LBS',city_id:1})
+    // const newairport=await noida.createAirport({name:'Noida international airport',code:'NOA',city_id:2})
+    // console.log(newairport);
+    // const data=await Airport.findByPk(1)
+    // const lal=await Airport.destroy({where:{code:'LBS'}});
+    const dn=await City.destroy({where:{
+        id:1
+    }})
+    // console.log(lal);
+
 })

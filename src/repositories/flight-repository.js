0039c077033c29fb7c7750f6dcdc399acc9be273
Creator Rama <ap410485@gmail.com>
2025-02-14 17:02:06 +0530

    created association for flights model

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -1,5 +1,6 @@
+const {Sequelize}=require('sequelize')
 const CrudRepository =require('./crud-repository')
-const {Flight}=require('../models')
+const {Flight,Airplane,Airport,City}=require('../models')
 class FlightRepository extends CrudRepository{
     constructor(){
         super(Flight)
@@ -9,7 +10,36 @@ class FlightRepository extends CrudRepository{
         console.log(filter,"\n");
         const response=await Flight.findAll({
             where:filter,
-            order:sort
+            order:sort,
+            include:[{
+                model:Airplane,
+                required:true,
+                as:'AirplaneDetail'               
+            },{
+                model:Airport,
+                required:true,
+               as:'departureAirport',
+                on:{
+                    col1:Sequelize.where(Sequelize.col("Flight.departureAirportId"),"=",Sequelize.col("departureAirport.code"))
+                },
+                include:{
+                    model:City,
+                    required:true
+                }
+            },
+            {
+                model:Airport,
+                required:true,
+               as:'arrivalAirport',
+                on:{
+                    col1:Sequelize.where(Sequelize.col("Flight.arrivalAirportId"),"=",Sequelize.col("arrivalAirport.code"))
+                },
+                include:{
+                    model:City,
+                    required:true
+                }
+            }
+        ]
         })
         return response
     }
